4566c1fa95dc9cd18777ffeb940ac064435828ab
Michael Anderson <anderson.j.michael@outlook.com>
2023-11-23 22:14:56 +0000

    Append stats output to file

--- getrl.py
+++ getrl.py
@@ -4,14 +4,23 @@ import subprocess
 import sys
 import time
 
-sec  = int(sys.argv[1])
+runtime  = int(sys.argv[1])
+interval = int(sys.argv[2])
 div = "--------------------"
 print("Getrl")
 print(div)
 
-while sec != 0:
+dp_cpu_stats = open("dp_cpu_stats.txt", "a")
+dp_mem_stats = open("dp_mem_stats.txt", "a")
+physical_port_stats = open("physical_port_stats", "a")
+tunnel_port_stats = open("tunnel_port_stats", "a")
+
+while runtime != 0:
     time.localtime()
-    subprocess.run(['su', 'admin', '-c', 'get dataplane cpu stats'])
-    sec -=  1
+    get_dp_cpu_stats=['su', 'admin', '-c', 'get dataplane cpu stats']
+    get_mem_stats=['su', 'admin', '-c', 'get dataplane memory stats']
+    subprocess.run(get_dp_cpu_stats, stdout=dp_cpu_stats)
+    subprocess.run(get_mem_stats, stdout=dp_mem_stats)
+    runtime -=  1
     print(div)
-    time.sleep(1)
+    time.sleep(interval)
