dbc3d9e5483d47eec4fe035d880382879a4ce403
Michael Anderson <anderson.j.michael@outlook.com>
2023-11-29 16:37:20 +0000

    Formatted stats to nested dict

--- getrl.py
+++ getrl.py
@@ -7,20 +7,39 @@ import time
 runtime  = int(sys.argv[1])
 interval = int(sys.argv[2])
 div = "--------------------"
+delimit="---"
+
+dp_stats=[]
+dp_core_stats=[]
+dp_core_stat={}
+
+dp_cpu_stats = open("dp_cpu_stats.txt", "a")
+get_dp_cpu_stats=['su', 'admin', '-c', 'get dataplane cpu stats']
+append_delimit=['echo', delimit]
 print("Getrl")
 print(div)
 
-dp_cpu_stats = open("dp_cpu_stats.txt", "a")
-dp_mem_stats = open("dp_mem_stats.txt", "a")
-physical_port_stats = open("physical_port_stats", "a")
-tunnel_port_stats = open("tunnel_port_stats", "a")
-
 while runtime != 0:
     time.localtime()
-    get_dp_cpu_stats=['su', 'admin', '-c', 'get dataplane cpu stats']
-    get_mem_stats=['su', 'admin', '-c', 'get dataplane memory stats']
+    print("Dumping DP CPU usage stats...")
     subprocess.run(get_dp_cpu_stats, stdout=dp_cpu_stats)
-    subprocess.run(get_mem_stats, stdout=dp_mem_stats)
+    subprocess.run(append_delimit, stdout=dp_cpu_stats)
     runtime -=  1
     print(div)
     time.sleep(interval)
+
+with open('dp_cpu_stats.txt') as dp_cpu_stats:
+    for line in dp_cpu_stats:
+        if 'CPU Usage' not in line:
+            if line.strip() and delimit not in line:
+                key, val  = line.strip().split(':')
+                dp_core_stat[key.strip()] = val.strip()
+            elif not line.strip():
+                dp_core_stats.append(dp_core_stat.copy())
+                dp_core_stat.clear()
+
+            if delimit in line:
+                dp_stats.append(dp_core_stats.copy())
+                dp_core_stats.clear()
+    print(dp_stats)
+    print('No. of entries,', len(dp_stats))
