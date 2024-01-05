ffae34da539bc6b5779972eb90163d5f09e6db1e
Michael Anderson <anderson.j.michael@outlook.com>
2024-01-05 11:34:17 +0000

    added build_list and write_to_cpu functions

--- getrl.py
+++ getrl.py
@@ -9,9 +9,9 @@ interval = int(sys.argv[2])
 div = "--------------------"
 delimit="---"
 
+dp_core_stat={}
 dp_stats=[]
 dp_core_stats=[]
-dp_core_stat={}
 
 dp_cpu_stats = open("dp_cpu_stats.txt", "a")
 get_dp_cpu_stats=['su', 'admin', '-c', 'get dataplane cpu stats']
@@ -19,37 +19,44 @@ append_delimit=['echo', delimit]
 print("Getrl")
 print(div)
 
-def convert_dict_val_to_int(dp_core_stat):
-    #print("Called:", convert_dict_val_to_int.__name__)
-    print(dp_core_stat)
+def clean_dict_val(dp_core_stat):
+    for key,val in dp_core_stat.items():
+        int_val = val.replace('pps', '')
+        int_val = val.replace('%', '')
+        dp_core_stat[key] = int_val.strip()
     return dp_core_stat
 
-#write stdoutput of command to file
-while runtime != 0:
-    time.localtime()
-    print("Dumping DP CPU usage stats...")
-    subprocess.run(get_dp_cpu_stats, stdout=dp_cpu_stats)
-    subprocess.run(append_delimit, stdout=dp_cpu_stats)
-    runtime -=  1
-    print(div)
-    time.sleep(interval)
+def write_cpu_stats_to_file(runtime, interval):
+    while runtime != 0:
+        time.localtime()
+        print("Dumping DP CPU usage stats...")
+        subprocess.run(get_dp_cpu_stats, stdout=dp_cpu_stats)
+        subprocess.run(append_delimit, stdout=dp_cpu_stats)
+        runtime -=  1
+        print(div)
+        time.sleep(interval)
+
+def build_list_of_dicts():
+    with open('dp_cpu_stats.txt') as dp_cpu_stats:
+        for line in dp_cpu_stats:
+            if 'CPU Usage' not in line:
+                if line.strip() and delimit not in line:
+                    key, val  = line.strip().split(':')
+                    dp_core_stat[key.strip()] = val.strip()
+                elif not line.strip():
+                    processed_dp_core_stat = clean_dict_val(dp_core_stat.copy())
+                    dp_core_stats.append(processed_dp_core_stat.copy())
+                    dp_core_stat.clear()
+                if delimit in line:
+                    dp_stats.append(dp_core_stats.copy())
+                    dp_core_stats.clear()
+        print(dp_stats)
+        print('Dumped DP CPU Stats', len(dp_stats), 'times in', runtime, 'seconds')
+
+write_cpu_stats_to_file(runtime, interval)
+build_list_of_dicts()
 
 
-with open('dp_cpu_stats.txt') as dp_cpu_stats:
-    for line in dp_cpu_stats:
-        if 'CPU Usage' not in line:
-            if line.strip() and delimit not in line:
-                key, val  = line.strip().split(':')
-                dp_core_stat[key.strip()] = val.strip()
-            elif not line.strip():
-                processed_dp_core_stat = convert_dict_val_to_int(dp_core_stat.copy())
-                dp_core_stats.append(processed_dp_core_stat.copy())
-                dp_core_stat.clear()
-            if delimit in line:
-                dp_stats.append(dp_core_stats.copy())
-                dp_core_stats.clear()
-    print(dp_stats)
-    print('No. of entries,', len(dp_stats))
 
 
 
